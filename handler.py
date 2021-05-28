26a8358d76972dc15adbe52d60deac4d478e7790
Rafael Hueb <rafaelhueb92@gmail.com>
2021-05-28 17:19:11 -0300

    bye hello world

--- handler.py
+++ handler.py
@@ -3,21 +3,7 @@ import boto3
 import uuid
 
 dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
-
 table = dynamodb.Table('usersTable')
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event["body"]
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
 
 def put_user(event,context):
 
